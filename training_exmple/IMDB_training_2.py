971f8910b129f8620c8687e91f7519f2d1bd6227
tuple_song <946851310@qq.com>
2020-01-18 18:28:26 +0800

    upload two examples

--- training_exmple/IMDB_training_2.py
+++ training_exmple/IMDB_training_2.py
@@ -1,12 +1,23 @@
+import numpy as np
 from keras.datasets import imdb
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
-word_index = imdb.get_word_index()
-reverse_word_index = dict(
-    [(value, key) for (key, value) in word_index.items()]
-)
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i , sequences in enumerate(sequences):
+        results[i, sequences] = 1.
+    return results
 
-decoded_review = ' '.join(
-    [reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
 
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+print(train_labels)
+
+# print(y_test)
+#
+# # print(x_train[0])
+# # print(x_test[0])
